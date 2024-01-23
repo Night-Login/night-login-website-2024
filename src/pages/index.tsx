0decdcda7309f549ac090fcf619b809612687c3f
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-23 19:44:17 +0700

    feat: add title on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import Hero from "@/components/Landing/Hero";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Night Login</title>
+    </Head>
     <main className="bg-neutral-1 min-h-screen">
       <Navbar />
       <Hero />
-    </main>
+    </main></>
   );
 }
