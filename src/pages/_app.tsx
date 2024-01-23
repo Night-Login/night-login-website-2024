749c651cc958c316c7250bea1c9ee42181082b59
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-23 19:39:26 +0700

    feat: add icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <Head>
+      <link rel="icon" href="/assets/images/Logo.png" />
+    </Head>
     <main className="font-poppins">
       <Component {...pageProps} />
     </main>
+    </>
   );
 }
