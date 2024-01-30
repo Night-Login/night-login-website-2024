cc33ba57a1a321c7389fe25dbfcbd2a96e8a4616
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-30 21:18:20 +0700

    feat: install and setup AOS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import AOS from "aos";
+import { useEffect } from "react";
+import "aos/dist/aos.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      once: false,
+      duration: 800,
+      easing: "ease-in-out",
+    });
+  }, []);
   return (
     <>
     <Head>
