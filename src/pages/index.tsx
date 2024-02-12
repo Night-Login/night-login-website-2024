630cc087fd3939e728be8491c4bfbe92121bf9ae
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-02-12 15:05:09 +0700

    feat: create placeholder for About page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/Landing/Hero";
 import Intro from "@/components/Landing/Intro";
 import Projects from "@/components/Landing/Projects";
-import Navbar from "@/components/Navbar";
 import Head from "next/head";
 
 export default function Home() {
@@ -11,7 +10,6 @@ export default function Home() {
         <title>Night Login</title>
       </Head>
       <main className="bg-neutral-1 w-full min-h-screen flex flex-col justify-center">
-        <Navbar />
         <Hero  />
         <Intro />
         <Projects />
