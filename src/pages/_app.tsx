71ac51540e568394ea079855d1a5ab231f4e5760
PetrusAriaa <petrusariacr25@gmail.com>
2024-02-23 23:27:04 +0700

    fix: hide Navbar for /requests path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,12 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import AOS from "aos";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 import "aos/dist/aos.css";
 import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const r = useRouter();
   useEffect(() => {
     AOS.init({
       once: false,
@@ -20,7 +22,11 @@ export default function App({ Component, pageProps }: AppProps) {
       <link rel="icon" href="/assets/images/Logo.png" />
     </Head>
     <main className="font-poppins">
-      <Navbar />
+      {
+          !r.pathname.includes("/requests") 
+          ? <Navbar /> 
+          : null
+      }
       <Component {...pageProps} />
     </main>
     </>
