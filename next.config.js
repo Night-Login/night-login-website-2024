9b4c9e100d2f911d4746dd4331dc602030679572
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-06-04 10:57:55 +0700

    feat: merge captcha

--- next.config.js
+++ next.config.js
@@ -2,8 +2,37 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["api.sandbox.midtrans.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.sandbox.midtrans.com",
+        port: "",
+        pathname: "/*",
+      },
+      {
+        protocol: "https",
+        hostname: "iai-captcha.vercel.app",
+        port: "",
+        pathname: "/captcha",
+      },
+    ],
   },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "api.sandbox.midtrans.com",
+  //       port: "",
+  //       pathname: "/*",
+  //     },
+  //     {
+  //       protocol: "https",
+  //       hostname: "iai-captcha.vercel.app",
+  //       port: "",
+  //       pathname: "/captcha",
+  //     },
+  //   ],
+  // }
 };
 
 module.exports = nextConfig;
--- src/components/Payment/PaymentPage.tsx
+++ src/components/Payment/PaymentPage.tsx
@@ -1,65 +1,82 @@
 import QRISLogo from "@/../public/assets/images/QRISLogo.png";
 import Logo from "@/../public/assets/images/Logo.png";
 import Image from "next/image";
-import { useState } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const PaymentPage = () => {
-    const [qrUrl, setQrUrl] = useState("");
+  const [qrUrl, setQrUrl] = useState("");
 
-    const handlePayment = () => {
-        axios.post(process.env.NEXT_PUBLIC_BACKEND_URL + "/api/v1/transaction/create-payment", {
-            orderId: localStorage.getItem("orderId"),
+  const handlePayment = () => {
+    axios
+      .post(
+        process.env.NEXT_PUBLIC_BACKEND_URL + "/api/v1/transaction/create-payment",
+        {
+          orderId: localStorage.getItem("orderId"),
         },
         {
-            headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
-        })
-        .then((res) => {
-            setQrUrl(res.data.qrLink);
-            console.log(res.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    };
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        setQrUrl(res.data.qrLink);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        setQrUrl("error");
+        console.log(err);
+      });
+  };
 
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="bg-white shadow-lg rounded-lg overflow-hidden max-w-3xl w-full mx-auto p-8">
-                <div className="grid grid-cols-1 sm:grid-cols-2 items-center gap-8">
-                    <div className="flex flex-col items-center p-6">
-                        <Image
-                            src={QRISLogo}
-                            alt="QRIS Logo"
-                            className="mb-6"
-                        />
-                        {qrUrl ? (
-                            <Image
-                                src={qrUrl}
-                                alt="QR Code"
-                                className="size-[300px]"
-                                width={160}
-                                height={160}
-                            />
-                        ) : (
-                            <div className="bg-slate-300 animate-pulse size-[300px]" />
-                        )}
-                    </div>
-                    <div className="flex flex-col justify-center p-6  border-gray-200">
-                        <h2 className="text-3xl font-semibold mb-1">Pembayaran DP</h2>
-                        <p className="text-4xl text-red font-bold mb-4">Rp15.000</p>
-                        <p className="font-light text-justify mt-1 max-w-full sm:max-w-[600px]">Untuk menyelesaikan pemesanan, lakukan pembayaran melalui QRIS dengan men-scan QR Code di samping menggunakan bank atau e-wallet yang telah support dengan QRIS.</p>
-                        <button
-                            onClick={handlePayment}
-                            className="mt-4 bg-red text-white py-2 px-4 rounded-lg hover:bg-red-600"
-                        >
-                            Generate QR Code
-                        </button>
-                    </div>
-                </div>
-            </div>
+  useEffect(() => {
+    handlePayment();
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white shadow-lg rounded-lg overflow-hidden max-w-3xl w-full mx-auto p-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 items-center gap-8">
+          <div className="flex flex-col items-center p-6">
+            <Image
+              src={QRISLogo}
+              alt="QRIS Logo"
+              className="mb-6"
+            />
+            {qrUrl === "error" ? (
+              <div className="bg-white size-[300px] flex justify-center items-center">
+                <span className="text-red">An Error Occured</span>
+              </div>
+            ) : qrUrl === "" ? (
+                <div className="bg-slate-300 size-[300px] flex justify-center items-center animate-pulse">
+                <span className="text-black">Loading</span>
+              </div>
+            ) : (
+              <Image
+                src={qrUrl}
+                alt="QR Code"
+                className="size-[300px]"
+              />
+            )}
+          </div>
+          <div className="flex flex-col justify-center p-6  border-gray-200">
+            <h2 className="text-3xl font-semibold mb-1">Pembayaran DP</h2>
+            <p className="text-4xl text-red font-bold mb-4">Rp15.000</p>
+            <p className="font-light text-justify mt-1 max-w-full sm:max-w-[600px]">
+              Untuk menyelesaikan pemesanan, lakukan pembayaran melalui QRIS dengan men-scan QR Code di samping
+              menggunakan bank atau e-wallet yang telah support dengan QRIS.
+            </p>
+            <button
+              onClick={handlePayment}
+              className="mt-4 bg-red text-white py-2 px-4 rounded-lg hover:bg-red-600"
+            >
+              Generate QR Code
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PaymentPage;
