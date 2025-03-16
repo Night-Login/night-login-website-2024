ab69c050b094ddea922a3238ed7855f7d9ef722e
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2025-03-17 00:54:00 +0700

    feat: add route for Talent Pool page in application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,6 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
     "/dashboard/history",
     "/dashboard/guide",
     "/dashboard/faq",
+    "/talent-pool"
   ];
   useEffect(() => {
     AOS.init({
